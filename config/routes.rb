f06c84f69fd9b05fa7102276a2cd1c3eaf1ce339
Nate <ndake11@gmail.com>
2017-02-19 12:35:18 +0100

    checkpoint 18 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+
+  resources :posts
+
   #this code creates HTTP GET routes for index and about views. Get requests
   #are used to retrieve information identified by the URL
-  get 'welcome/index'
-
-  get 'welcome/about'
+  get 'about' => 'welcome#about'
 
   #root method allows the declaration of a default page the app loads when
   #navigating to the home page URL. Root takes a hash as an argument (implied
