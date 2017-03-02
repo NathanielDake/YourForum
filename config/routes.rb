a6dc0e03fa254fde54c927f7acbcebe0ebe111bf
Nate <ndake11@gmail.com>
2017-03-02 13:50:47 +0100

    assignment 25 complete

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create]
 
+  post 'users/confirmation' => 'users#confirmation'
 
 
   #this code creates HTTP GET routes for index and about views. Get requests
