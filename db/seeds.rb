ef2ce07d38522e5f978626352205340c6f99bd1d
Nate <ndake11@gmail.com>
2017-03-08 11:35:51 -0500

    checkpoint 28: completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,11 +34,19 @@ posts = Post.all
   )
 end
 
-user = User.first
-user.update_attributes!(
-  email: 'ndake11@gmail.com',
-  password: 'Thunder10!'
+admin = User.create!(
+    name: 'Admin User',
+    email: 'admin@example.com',
+    password: 'helloworld',
+    role: 'admin'
 )
+
+member = User.create!(
+    name: 'Member User',
+    email: 'member@example.com',
+    password: 'helloworld'
+)
+
 
 puts "Seed finished"
 puts "#{User.count} users created"
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,7 +9,6 @@ RSpec.describe User, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
 
-
   #shoulda tests for email
   it { is_expected.to validate_presence_of(:email) }
   it { is_expected.to validate_uniqueness_of(:email) }
@@ -24,8 +23,56 @@ RSpec.describe User, type: :model do
   describe "attributes" do
     it "should have name and email attributes" do
         expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
+    end
+
+    it "responds to role" do
+      expect(user).to respond_to(:role)
+    end
+
+    # #2
+    it "responds to admin?" do
+      expect(user).to respond_to(:admin?)
+    end
+
+    # #3
+    it "responds to member?" do
+      expect(user).to respond_to(:member?)
+    end
+  end
+
+  describe "roles" do
+    # #4
+    it "is member by default" do
+      expect(user.role).to eql("member")
+    end
+
+    # #5
+    context "member user" do
+      it "returns true for #member?" do
+        expect(user.member?).to be_truthy
+      end
+
+      it "returns false for #admin?" do
+        expect(user.admin?).to be_falsey
       end
     end
+
+    # #6
+    context "admin user" do
+      before do
+        user.admin!
+      end
+
+      it "returns false for #member?" do
+        expect(user.member?).to be_falsey
+      end
+
+      it "returns true for #admin?" do
+        expect(user.admin?).to be_truthy
+      end
+    end
+  end
+
 
   describe "invalid user" do
     let(:user_with_invalid_name) { User.new(name: "", email: "user@bloccit.com") }
