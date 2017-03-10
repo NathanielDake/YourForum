cd6507fe08c437dba19781779c33c7d7ca1232be
Nate <ndake11@gmail.com>
2017-03-09 21:29:04 -0500

    checkpoint 30 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,9 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   belongs_to :user
   has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
 
-  default_scope { order('created_at DESC') }
+  default_scope { order('rank DESC') }
 
 
   validates :title, length: { minimum: 5 }, presence: true
@@ -11,4 +12,28 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true
 
+
+  #find all the upvotes for a post by passing value: 1 to where. where(value: 1) fetches only the
+  #votes with a value of 1. Count is then called on the collection
+  def up_votes
+    self.votes.where(value: 1).count
+  end
+
+  #find all the downvotes for a post by passing value: -1 to where. where(value: -1) fetches only the
+  #votes with a value of -1. Count is then called on the collection
+  def down_votes
+    self.votes.where(value: -1).count
+  end
+
+  #This is using ActiveRecords sum method to add the value of all the given post's votes. Passing :value
+  #to sum tells it what attribute to sum in the collection
+  def points
+    self.votes.sum(:value)
+  end
+
+  def update_rank
+    age_in_days = (created_at - Time.new(1970,1,1))/ 1.day.seconds
+    new_rank = points + age_in_days
+    update_attribute(:rank, new_rank)
+  end
 end
